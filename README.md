ce7449a61b3056069e9ec27df66dfc75b6299694
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2022-01-03 16:34:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Automate ScreenShot
+
+#### Technology
+  * Node.js >= 10
+  * Web Driver Selenium >= 4
+
+### Runing Project
+Access to folder **selenium-automation-js**
+
+Install dependencies
+``` npm i ```
+
+After of install the dependencies
+``` npm run dev ```
---
+++ index.js
@@ -0,0 +1,40 @@
+// drive chrome
+// require('chromedriver')
+
+// drive firefox
+require("geckodriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
+// const fs = require("fs");
+
+async function test() {
+  let driver = await new Builder()
+    .forBrowser("firefox")
+    .build();
+
+  /* try {
+    fs.mkdirSync('./screenShot')
+  } catch(err) {
+    if(err.code !== 'EEXIST') console.error(err)
+  } */
+
+  try {
+
+    await driver.get("https://www.google.com");
+
+    await driver.findElement(By.name('q')).sendKeys('Selenium', Key.ENTER);
+
+    await driver.takeScreenshot().then((image) => {
+      fs.writeFileSync("./screenShot/searchSelenium.png", image, "base64");
+    });
+
+    await driver.get("https://www.facebook.com");
+    await driver.takeScreenshot().then((image) => {
+      fs.writeFileSync("./screenShot/facebook.png", image, "base64");
+    });
+  } finally {
+    await driver.quit();
+  }
+}
+
+test();
+
