000f99475b68e9c94f026e76c2964015905f2aa9
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2022-01-04 10:39:55 -0300

    screenshot after search google

--- index.js
+++ index.js
@@ -4,18 +4,18 @@
 // drive firefox
 require("geckodriver");
 const { Builder, By, Key, until } = require("selenium-webdriver");
-// const fs = require("fs");
+const fs = require("fs");
 
 async function test() {
   let driver = await new Builder()
     .forBrowser("firefox")
     .build();
 
-  /* try {
+  try {
     fs.mkdirSync('./screenShot')
   } catch(err) {
     if(err.code !== 'EEXIST') console.error(err)
-  } */
+  }
 
   try {
 
@@ -23,14 +23,18 @@ async function test() {
 
     await driver.findElement(By.name('q')).sendKeys('Selenium', Key.ENTER);
 
+    const url = await driver.getCurrentUrl()
+
+    await driver.get(`${url}search?q=Selenium`)
+
     await driver.takeScreenshot().then((image) => {
       fs.writeFileSync("./screenShot/searchSelenium.png", image, "base64");
     });
 
-    await driver.get("https://www.facebook.com");
+   /*  await driver.get("https://www.facebook.com");
     await driver.takeScreenshot().then((image) => {
       fs.writeFileSync("./screenShot/facebook.png", image, "base64");
-    });
+    }); */
   } finally {
     await driver.quit();
   }
